a36acdbfdfda81099195181d8cbb32a00a662d83
ShreyashPatodia <patodiashreyash32@gmail.com>
2017-03-25 03:43:19 +1100

    Refactor to make more sense wrt to OO design

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ShreyashPatodia on 25/03/17.
+ */
+public class Game {
+
+    public static void main(String []args) {
+
+        Board board = new Board();
+        board.getInput();
+    }
+
+}
