f5c357bafa8cd85dc461b1487624eb81483e87db
Bermuda_UT <hoso1312@gmail.com>
2017-04-07 08:23:08 +1000

    login details update

--- src/com/teammaxine/game/elements/Cell.java
+++ src/com/teammaxine/game/elements/Cell.java
@@ -1,7 +1,7 @@
 /**
  * Created by Shreyash Patodia and Max Lee (Ho Suk Lee).
  * Student numbers: Shreyash - 767336, Max Lee - 719577
- * Login: Shreyash - spatodia, Max - hlee39
+ * Login: Shreyash - spatodia, Max - hol2
  * Subject: COMP30024 Artificial Intelligence.
  * Semester 1, 2017.
  */
