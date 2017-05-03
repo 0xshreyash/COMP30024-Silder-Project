d692798cf52486bbf0a113edc7af685bda7e63de
ShreyashPatodia <spatodia@student.unimelb.edu.au>
2017-05-04 07:33:19 +1000

    Update gitignore

--- src/com/teammaxine/agent/JungEun.java
+++ src/com/teammaxine/agent/JungEun.java
@@ -1,10 +1,32 @@
 package com.teammaxine.agent;
 
+
+
+
+import aiproj.slider.Move;
+import aiproj.slider.SliderPlayer;
+
 /**
  * Created by noxm on 17/04/17.
  * Base line AI
  * (dictates everything,
- *  fails to fire a missile)
+ * fails to fire a missile)
  */
-public class JungEun {
+public class JungEun implements SliderPlayer {
+    @Override
+    public void init(int dimension, String board, char player) {
+
+
+    }
+
+    @Override
+    public void update(Move move) {
+
+    }
+
+    @Override
+    public Move move() {
+        return null;
+    }
 }
+
