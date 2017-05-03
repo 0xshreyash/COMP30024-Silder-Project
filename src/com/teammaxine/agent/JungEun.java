6d95971bcebc1431f4ed8c9a2276b36526f595ff
ShreyashPatodia <spatodia@student.unimelb.edu.au>
2017-05-04 08:36:31 +1000

    Add board creation to JungEun

--- src/com/teammaxine/agent/JungEun.java
+++ src/com/teammaxine/agent/JungEun.java
@@ -5,6 +5,14 @@ package com.teammaxine.agent;
 
 import aiproj.slider.Move;
 import aiproj.slider.SliderPlayer;
+import com.teammaxine.board.elements.Board;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by noxm on 17/04/17.
@@ -16,7 +24,28 @@ public class JungEun implements SliderPlayer {
     @Override
     public void init(int dimension, String board, char player) {
 
+        Board myBoard = createBoard(dimension, board);
+        //System.out.println(myBoard);
+    }
 
+    public Board createBoard(int dimension, String board) {
+
+        ArrayList<String> boardMapping = new ArrayList<>();
+        BufferedReader buffer = new BufferedReader(new StringReader(board));
+        for (int row = 0; row < dimension; row++) {
+            String line = "";
+            try {
+                line = buffer.readLine();
+            }
+            catch(IOException e) {
+                System.err.println(e);
+            }
+            // remove spaces and add to the head of the list
+            line = line.replaceAll("\\s", "");
+            boardMapping.add(0, line);
+        }
+
+        return new Board(boardMapping);
     }
 
     @Override
