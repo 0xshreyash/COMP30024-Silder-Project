8dbc7481ea0387cc6b5823fdba135d0a29082b6e
Bermuda_UT <hoso1312@gmail.com>
2017-05-10 18:41:54 +1000

    score shit

--- src/com/teammaxine/agent/Xena.java
+++ src/com/teammaxine/agent/Xena.java
@@ -2,6 +2,7 @@ package com.teammaxine.agent;
 
 import aiproj.slider.Move;
 import aiproj.slider.SliderPlayer;
+import com.teammaxine.board.helpers.MonteCarloScorer;
 import com.teammaxine.board.helpers.Scorer;
 import com.teammaxine.strategies.MonteCarlo;
 import com.teammaxine.strategies.Strategy;
@@ -12,7 +13,7 @@ import com.teammaxine.strategies.Strategy;
 public class Xena extends Agent {
     @Override
     public Move move() {
-        Strategy myStrategy = new MonteCarlo(getPlayer(), new Scorer());
+        Strategy myStrategy = new MonteCarlo(getPlayer(), new MonteCarloScorer());
 
         Move toMake = myStrategy.findMove(this.getMyBoard(), 0);
         this.update(toMake, this.getPlayer());
